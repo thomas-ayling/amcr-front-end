32653c8c39bdc2c8eb6c1762f80c434aeddfc357
thomas-ayling <thomas.ayling@ecs.co.uk>
2022-11-29 15:36:25 +0000

    Front-end start point

---
+++ client/src/MainRoutes.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import Wiki from './pages/Wiki';
+import Contacts from './pages/Contacts';
+import CaseStudies from './pages/CaseStudies';
+import Library from './pages/Library';
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<HomePage />} />
+      <Route exact path='/wiki' element={<Wiki />} />
+      <Route exact path='/contact' element={<Contacts />} />
+      <Route exact path='/case-studies' element={<CaseStudies />} />
+      <Route exact path='/library' element={<Library />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
