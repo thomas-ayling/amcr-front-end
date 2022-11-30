f4c69b8d94730a29d24e33d8a0b523c41781fa48
CSCairney-GL <charles.cairney@ecs.co.uk>
2022-11-30 16:49:01 +0000

    Spacing for top section complete.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="container">
+        <section className="top-area">
+          {/* Column for Global Logic Logo */}
+          <div className="footer-logo">
+            <h1>Logo here</h1>
+          </div>
+          {/* Column for internal page links */}
+          <div className="footer-pages">
+            <div className="internal-pages">
+              <div className="footer-link"><Link to={`/onboarding/profile`}>Case Studies</Link></div>
+              <div className="footer-link"><Link to={`/Wiki`}>Wiki</Link></div>
+              <div className="footer-link"><Link to={`/Designs`}>Design Systems</Link></div>
+              <div className="footer-link"><Link to={`/library`}>Reference Library</Link></div>
+              <div className="footer-link"><Link to={`/contacts`}>Contacts</Link></div>
+            </div>
+          </div>
+          {/* Column for admin panel */}
+          <div className="admin-panel">
+            <Link to={`/admin`}>Admin Panel</Link>
+          </div>
+        </section>
+        <section className="bottom-area">
+          {/* Column for Social Media Links */}
+          <div className="footer-social">
+            <link></link>
+            <link></link>
+            <link></link>
+            <link></link>
+          </div>
+        </section>
+        <hr />
+      </div>
+    </div>
   )
 }
 
