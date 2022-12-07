e9a9653653d16ea768e94f119ebd50c2bdacd184
jab-khanGL <jabaran.khan@ecs.co.uk>
2022-12-07 14:40:05 +0000

    updated and cleaned files

--- client/src/components/carousel/MainCarousel.jsx
+++ client/src/components/carousel/MainCarousel.jsx
@@ -14,41 +14,34 @@ function MainCarousel({images}) {
      timeOut =  autoPlay && setTimeout(() => {
             slideRight();
 
-        }, 5000);
-    }
+            }, 5000);
+        } //timer function- disables error as timer doesn't need to be stored
     )
 
     const slideRight = () => {
         setCurrent(current === images.length - 1 ? 0 : current + 1);
-    }
+    } // slide right and current checks what image it is on - it will go back to one if it reaches the end
     
     
-    console.log(current, timeOut);
+    console.log(current, timeOut); //for testing purposes
+    
     return (
-    <div className="carousel-background">
-    <div className="carousel"  onMouseEnter={() => {setAutoPlay(false);
-        clearTimeout(timeOut);  
+        <div className="carousel-background">
+            <div className="carousel"  onMouseEnter={() => {setAutoPlay(false); //checks 
+            clearTimeout(timeOut);  
 
      
-    }} onMouseLeave={() => {setAutoPlay(true)}}> 
+            }} onMouseLeave={() => {setAutoPlay(true)}}> 
     
-        <div className="carousel_wrapper">
-        {images.map((image, index)=>{
-            return( <div key={index} className={index===current ? "carousel_card carousel_card-active" : "carousel_card"} >
+            <div className="carousel_wrapper">
+             {images.map((image, index)=>{
+                return( <div key={index} className={index===current ? "carousel_card carousel_card-active" : "carousel_card"} >
                 <img className="card_image" src={image.image} alt =""/>
-                
-                {/* <div className="card_text">{image.overview.substring(0, 200)}</div> */}
-
                 <div className="card_overlay">
-                
-                   
-                    {/* <h3 className="card_text">{image.overview}</h3> */}
                     </div>
-                    
-                    <div className="carousel_pagination">
-                        
-                        {images.map((_,index)=>{
-                            return(
+                         <div className="carousel_pagination">
+                             {images.map((_,index)=>{
+                                return(
                                 <div key={index}
                                     className={index === current ? "pagination_dot pagination_dot-active" : "pagination_dot"
                                     }
@@ -68,18 +61,18 @@ function MainCarousel({images}) {
 
 
             </div>
-            <div className="carousel_wrapper_second">
-            {images.map((image, index)=>{
-                return(<div key={index} className={index===current ? "carousel_card_textbox carousel_card-active" : "carousel_card_textbox"}>
+                <div className="carousel_wrapper_second">
+                    {images.map((image, index)=>{
+                    return(<div key={index} className={index===current ? "carousel_card_textbox carousel_card-active" : "carousel_card_textbox"}>
                 
-                <div className="card_text"> 
-                <h1>{image.title}</h1>
-                <h2 className ="card_title2">{image.overview.substring(0, 100)}</h2>
-                <a className="link_text" href={image.target} rel="noreferrer">Find out more </a>
-                {/* <a href="" rel="noreferrer">{image.target}FAQ</a> */}
+                    <div className="card_text"> 
+                    <h1>{image.title}</h1>
+                    <h2 className ="card_title2">{image.overview.substring(0, 100)}</h2>
+                    <a className="link_text" href={image.target} rel="noreferrer">Find out more </a>
+              
                 
                 </div>
-                </div>) 
+            </div>) 
 
 
             }
