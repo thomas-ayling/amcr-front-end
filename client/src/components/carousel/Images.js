331102a887c702395e5d5901321eeac93156c26d
jab-khanGL <jabaran.khan@ecs.co.uk>
2022-12-09 11:08:16 +0000

    fixed carousel and merging to dev

--- client/src/components/carousel/Images.js
+++ client/src/components/carousel/Images.js
@@ -1,19 +1,19 @@
 export const images =[
     {   
-        image: "/images/sky1.jpg",
+        image: "/images/City1.jpeg",
         title: "Sky1",
-        target: "https://www.google.co.uk/",
+        target: "/wiki",
         overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." , 
     },
     {   
-        image: "/images/sky2.jpg",
+        image: "/images/City2.jpeg",
         title: "Sky2",
         overview: "Hello and welcome to blah",
         target: "https://www.google.co.uk/",
        
     },
     {   
-        image: "/images/sky3.jpg",
+        image: "/images/City3.jpeg",
         title: "Sky3",
         target: "https://www.google.co.uk/",
         overview: "Hello and welcome to blah",
--- client/src/components/carousel/MainCarousel.jsx
+++ client/src/components/carousel/MainCarousel.jsx
@@ -54,7 +54,7 @@ function MainCarousel({images}) {
                                 )
                         })}
                         </div>
-                   
+
                     </div>
   
 
