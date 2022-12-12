0f5dbda5962d2c547d1d8743e3b2834b00a6ec01
thomas-ayling <thomas.ayling@ecs.co.uk>
2022-12-12 09:43:53 +0000

    Added checks to ensure uploaded files aren't too large and added name checks. pushing to QA

--- src/components/contact-component/input-components/shared-input-components/AttachmentInput.jsx
+++ src/components/contact-component/input-components/shared-input-components/AttachmentInput.jsx
@@ -4,6 +4,15 @@ import FileIcon from '../../../../resources/icons/file-icon.png';
 const AttachmentInput = ({ setAttachment }) => {
   const [fileName, setFileName] = useState('No file');
 
+  const setAttachmentWithChecks = (file) => {
+    if (file.size > 5242880) {
+      alert('This file is too large. Files must be under 5MB to be uploaded.');
+      return;
+    }
+    setAttachment(file);
+    setFileName(file.name);
+  };
+
   return (
     <div className='attachment-input-container'>
       <label htmlFor='attachment-input' className='attachment-input-label'>
@@ -13,17 +22,7 @@ const AttachmentInput = ({ setAttachment }) => {
       <span id='chosen-file' className='chosen-file'>
         {fileName}
       </span>
-      <input
-        type='file'
-        name='attachment-input'
-        id='attachment-input'
-        className='attachment-input contact-form-input'
-        onChange={(e) => {
-          setAttachment(e.target.files[0]);
-          setFileName(e.target.files[0].name);
-        }}
-        hidden
-      />
+      <input type='file' name='attachment-input' id='attachment-input' className='attachment-input contact-form-input' onChange={(e) => setAttachmentWithChecks(e.target.files[0])} hidden />
     </div>
   );
 };
