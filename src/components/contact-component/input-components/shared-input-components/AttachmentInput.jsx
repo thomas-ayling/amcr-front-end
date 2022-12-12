1cea50a0556ec0d75ddd72aef2bc66246c040596
thomas-ayling <thomas.ayling@ecs.co.uk>
2022-12-12 10:19:35 +0000

    Fixed text overlap wiht attachment input on smaller screens

--- src/components/contact-component/input-components/shared-input-components/AttachmentInput.jsx
+++ src/components/contact-component/input-components/shared-input-components/AttachmentInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import FileIcon from '../../../../resources/icons/file-icon.png';
 
 const AttachmentInput = ({ setAttachment }) => {
-  const [fileName, setFileName] = useState('No file');
+  const [fileName, setFileName] = useState('Add file');
 
   const setAttachmentWithChecks = (file) => {
     if (file.size > 5242880) {
@@ -14,16 +14,15 @@ const AttachmentInput = ({ setAttachment }) => {
   };
 
   return (
-    <div className='attachment-input-container'>
+    <>
       <label htmlFor='attachment-input' className='attachment-input-label'>
-        Choose file &nbsp;
+        <span id='chosen-file' className='chosen-file'>
+          {fileName}
+        </span>
         <img src={FileIcon} alt='' id='file-icon' />
       </label>
-      <span id='chosen-file' className='chosen-file'>
-        {fileName}
-      </span>
       <input type='file' name='attachment-input' id='attachment-input' className='attachment-input contact-form-input' onChange={(e) => setAttachmentWithChecks(e.target.files[0])} hidden />
-    </div>
+    </>
   );
 };
 
