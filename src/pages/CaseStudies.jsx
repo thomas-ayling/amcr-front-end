1ba40f1c69686ec0a0cc56b8cbe0db07664ec3a0
thomas-ayling <thomas.ayling@ecs.co.uk>
2022-12-13 15:17:47 +0000

    added component to pages

--- src/pages/CaseStudies.jsx
+++ src/pages/CaseStudies.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import ContactComponent from '../components/contact-component/ContactComponent';
 
 const CaseStudies = () => {
   return (
     <Container fluid className='page-section'>
       <div>CaseStudies</div>
+      <ContactComponent type='case-study' />
     </Container>
   );
 };
 
 export default CaseStudies;
-
-export default CaseStudies
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import ContactComponent from '../components/contact-component/ContactComponent';
 
 const Library = () => {
   return (
     <Container fluid className='page-section'>
       <div>Library</div>
+      <ContactComponent type='library' />
     </Container>
   );
 };
 
 export default Library;
-
-export default Library
