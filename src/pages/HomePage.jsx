034565dd549cb25734804e4c8bedf6e494e24b06
jab-khanGL <jabaran.khan@ecs.co.uk>
2022-12-13 10:02:49 +0000

    fixed majority of css issues

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -28,35 +28,21 @@ const HomePage = () => {
       
       <div>
         <h1>
-          Lorem ipsum dolor sit amet. Sed quam repellendus est tempora consequatur nam officia neque nam officiis illum? Hic libero consectetur et deserunt sint ut voluptatibus
-          inventore et ullam galisum quo porro rerum eum quia assumenda est illo laudantium! Eos galisum rerum sit quod voluptas in cumque natus et odit omnis. Non nulla nulla quo
-          galisum facilis et neque alias aut fugiat beatae? Et internos fugit nam voluptatem sequi ut voluptas rerum id ipsa minima? Ut laboriosam deserunt a dicta nihil ea quis
-          totam ab odit sequi. Ut quos debitis est harum rerum eos velit quis est obcaecati voluptas sed neque adipisci eum assumenda cupiditate qui voluptas velit? Lorem ipsum
-          dolor sit amet. Qui deserunt repellat qui esse ipsum est consequatur inventore in vero corporis ut quos saepe. Et deserunt dolores vel enim dignissimos aut iusto ipsa sit
-          quibusdam tenetur qui asperiores consequatur. Qui modi obcaecati ut rerum soluta et mollitia dolorem ea consequatur numquam sed dolorem dicta eum eligendi earum vel
-          facere voluptates. Qui dicta asperiores qui autem architecto sed obcaecati officia ut perferendis molestiae est animi odit. Est porro quia ut dignissimos quis eos earum
-          nihil. Et nostrum necessitatibus est quos tempore ab voluptate possimus ut consequuntur mollitia et culpa deserunt. Qui provident modi eum sint asperiores ut quod
-          excepturi. Et eius quasi in explicabo galisum qui blanditiis tempore. Ea galisum autem sit inventore aliquid ex cumque quia est eaque inventore et distinctio consequatur
-          et unde consectetur et inventore fugiat. Lorem ipsum dolor sit amet. Sed quam repellendus est tempora consequatur nam officia neque nam officiis illum? Hic libero
-          consectetur et deserunt sint ut voluptatibus inventore et ullam galisum quo porro rerum eum quia assumenda est illo laudantium! Eos galisum rerum sit quod voluptas in
-          cumque natus et odit omnis. Non nulla nulla quo galisum facilis et neque alias aut fugiat beatae? Et internos fugit nam voluptatem sequi ut voluptas rerum id ipsa minima?
-          Ut laboriosam deserunt a dicta nihil ea quis totam ab odit sequi. Ut quos debitis est harum rerum eos velit quis est obcaecati voluptas sed neque adipisci eum assumenda
-          cupiditate qui voluptas velit? Lorem ipsum dolor sit amet. Qui deserunt repellat qui esse ipsum est consequatur inventore in vero corporis ut quos saepe. Et deserunt
-          dolores vel enim dignissimos aut iusto ipsa sit quibusdam tenetur qui asperiores consequatur. Qui modi obcaecati ut rerum soluta et mollitia dolorem ea consequatur
-          numquam sed dolorem dicta eum eligendi earum vel facere voluptates. Qui dicta asperiores qui autem architecto sed obcaecati officia ut perferendis molestiae est animi
-          odit. Est porro quia ut dignissimos quis eos earum nihil. Et nostrum necessitatibus est quos tempore ab voluptate possimus ut consequuntur mollitia et culpa deserunt. Qui
-          provident modi eum sint asperiores ut quod excepturi. Et eius quasi in explicabo galisum qui blanditiis tempore. Ea galisum autem sit inventore aliquid ex cumque quia est
-          eaque inventore et distinctio consequatur et unde consectetur et inventore fugiat. Lorem ipsum dolor sit amet. Sed quam repellendus est tempora consequatur nam officia
-          neque nam officiis illum? Hic libero consectetur et deserunt sint ut voluptatibus inventore et ullam galisum quo porro rerum eum quia assumenda est illo laudantium! Eos
-          galisum rerum sit quod voluptas in cumque natus et odit omnis. Non nulla nulla quo galisum facilis et neque alias aut fugiat beatae? Et internos fugit nam voluptatem
-          sequi ut voluptas rerum id ipsa minima? Ut laboriosam deserunt a dicta nihil ea quis totam ab odit sequi. Ut quos debitis est harum rerum eos velit quis est obcaecati
-          voluptas sed neque adipisci eum assumenda cupiditate qui voluptas velit? Lorem ipsum dolor sit amet. Qui deserunt repellat qui esse ipsum est consequatur inventore in
-          vero corporis ut quos saepe. Et deserunt dolores vel enim dignissimos aut iusto ipsa sit quibusdam tenetur qui asperiores consequatur. Qui modi obcaecati ut rerum soluta
-          et mollitia dolorem ea consequatur numquam sed dolorem dicta eum eligendi earum vel facere voluptates. Qui dicta asperiores qui autem architecto sed obcaecati officia ut
-          perferendis molestiae est animi odit. Est porro quia ut dignissimos quis eos earum nihil. Et nostrum necessitatibus est quos tempore ab voluptate possimus ut consequuntur
-          mollitia et culpa deserunt. Qui provident modi eum sint asperiores ut quod excepturi. Et eius quasi in explicabo galisum qui blanditiis tempore. Ea galisum autem sit
-          inventore aliquid ex cumque quia est eaque inventore et distinctio consequatur et unde consectetur et inventore fugiat.
+          Welcome to Global Logic!
         </h1>
+        <h2>We are here to provide you the best experience in finding what you are looking for.
+          This could be anything in our library or perhaps a case study that you are interested in. 
+          If there isn't something that you are looking for. Please feel free to contact us and propose an improvement.
+        </h2>
+      </div>
+      <div >
+      <iframe  width="100%" height="600" src="https://www.youtube.com/embed/HII9KGHSGYE" 
+      title="YouTube video player" frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+      gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+      <div className='page'>
+        <MainCarousel images={MainCarouselImages} />
       </div>
     </Container>
   );
