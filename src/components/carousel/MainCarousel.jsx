ff635d4d8ef6a087d2f50b9619464493278e1cdc
jab-khanGL <jabaran.khan@ecs.co.uk>
2022-12-19 16:05:24 +0000

    swipe function working - one bug remains in terms of keys

--- src/components/carousel/MainCarousel.jsx
+++ src/components/carousel/MainCarousel.jsx
@@ -3,9 +3,12 @@ import { useEffect, useState } from 'react';
 import CarouselCards from './shared-components/CarouselCards';
 import CarouselTextbox from './shared-components/CarouselTextbox';
 
+
+
 const MainCarousel = ({ images }) => {
   const [current, setCurrent] = useState(0);
   const [autoPlay, setAutoPlay] = useState(true);
+  
 
   let timeOut = null;
 
@@ -13,11 +16,12 @@ const MainCarousel = ({ images }) => {
 
   useEffect(
     () => {
-      timeOut =
+     let timeOut =
         autoPlay &&
         setTimeout(() => {
           slideRight();
         }, 5000);
+        return () => clearTimeout(timeOut);
     }
     //timer function- disables error as timer doesn't need to be stored
   );
@@ -26,6 +30,52 @@ const MainCarousel = ({ images }) => {
     setCurrent(current === images.length - 1 ? 0 : current + 1);
   }; // slide right and current checks what image it is on - it will go back to one if it reaches the end
 
+  const slideLeft = () => {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  const [touchStart, setTouchStart] = useState(null)
+  const [touchEnd, setTouchEnd] = useState(null)
+  const minSwipeDistance = 100
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+    clearTimeout(timeOut)
+    setAutoPlay(false)
+  
+  }
+  
+  const onTouchMove = (e) =>{ setTouchEnd(e.targetTouches[0].clientX)
+      
+      
+  }
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) 
+      return
+    
+    const distance = touchStart - touchEnd 
+    
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+   
+
+    if (isLeftSwipe){
+      slideLeft();
+      
+     
+    }
+
+
+    if (isRightSwipe){
+      slideRight();
+    }
+
+
+   
+  }
+  
+
   const handleMouseEnter = () => {
     setAutoPlay(false);
     clearTimeout(timeOut);
@@ -33,12 +83,14 @@ const MainCarousel = ({ images }) => {
 
   return (
     <div className='carousel-background'>
-      <div className='carousel' onMouseEnter={handleMouseEnter} onMouseLeave={() => setAutoPlay(true)}>
+      <div className='carousel' onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} onMouseEnter={handleMouseEnter} onMouseLeave={() => setAutoPlay(true)}>
         <div className='carousel_wrapper'>
           <CarouselCards images={images} current={current} paginationDots={paginationDots} />
+          
         </div>
         <div className='carousel_wrapper_second'>
-          <CarouselTextbox images={images} current={current} />
+          <CarouselTextbox images={images} current={current}/>
+          
         </div>
       </div>
     </div>
---
+++ src/components/carousel/shared-components/CarouselTimer.jsx
@@ -0,0 +1,26 @@
+
+import { useEffect, useState } from 'react';
+
+const CarouselTimer = ({ seconds }) => {
+  // initialize timeLeft with the seconds prop
+  const [timeLeft, setTimeLeft] = useState(seconds);
+
+  useEffect(() => {
+    // exit early when we reach 0
+    if (!timeLeft) return;
+
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft]);
+
+  return CarouselTimer;
+};
+  export default CarouselTimer;
