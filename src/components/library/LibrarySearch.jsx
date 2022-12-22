d734df3539f08d827e741d9b82d58992e9c415dd
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2022-12-22 15:29:24 +0000

    JSON population and search/filter ability working.

--- src/components/library/LibrarySearch.jsx
+++ src/components/library/LibrarySearch.jsx
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState } from "react";
 import BookCover from "./TestBookCover.png";
 import "./LibrarySearch.css";
-// import {BsFillArrowDownCircleFill} from 'react-icons/bs';
-
-// *** future axios implementation - Once database is populated *** //
-// const getData = async () => {
-//     const {data} = await axios.get('https://databaseUrl/books/getAll');
-//     setBooks(data);
-// }
-// *** future axios implementation - Once database is populated *** //
+import dataMock from "./Books.json";
 
 const LibrarySearch = () => {
-//   const [books, setBooks] = useState([]);
   const [searchInput, setSearchInput] = useState("");
-//   const [dropDownStatus, setDropDownStatus] = useState(false);
-//   const [searchParam] = useState(["name", "author", "genre"]);
+  const [searchParam] = useState(["name", "author", "genre"]);
+  const [filterParam, setFitlerParam] = useState("");
 
-  // Testing Library sudo Database
-  var fakebooks = [
-    { id: 1, name: "book One", genre: "Devops", author: "Charles", reader: "John", out: false, cover: { BookCover } },
-    { id: 2, name: "book Two", genre: "Java", author: "John", reader: "", out: false, cover: { BookCover } },
-    { id: 3, name: "book Three", genre: "Devops", author: "Steve", reader: "Kentaro Miura", out: true, cover: { BookCover } },
-    { id: 4, name: "book Four", genre: "Python", author: "Alex", reader: "Kevin", out: true, cover: { BookCover } },
-    { id: 5, name: "book Five", genre: "Management", author: "Tom", reader: "Smith", out: true, cover: { BookCover } },
-    { id: 6, name: "book Six", genre: "Python", author: "Zoriana", reader: "tanjiro", out: false, cover: { BookCover } },
-    { id: 7, name: "book Seven", genre: "Java", author: "Kya", reader: "Haruki Murakami", out: false, cover: { BookCover } },
-    { id: 8, name: "book Eight", genre: "Java", author: "Stanislav", reader: "John", out: true, cover: { BookCover } },
-    { id: 9, name: "book Nine", genre: "Buisness", author: "Jab", reader: "Saitama", out: false, cover: { BookCover } },
-  ];
-
-//   useEffect(() => {
-//     getData();
-//     setBooks(fakebooks);
-//   });
+  function search(items) {
+    return items.filter((item) => {
+        if (item.genre === filterParam) {
+            return searchParam.some((newItem) => {
+                return (
+                    item[newItem]
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(searchInput.toLowerCase()) > -1
+                );
+            });
+        } else if (filterParam === "") {
+            return searchParam.some((newItem) => {
+                return (
+                    item[newItem]
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(searchInput.toLowerCase()) > -1
+                );
+            });
+        } 
+    });
+  }
 
   return (
     <div className='Library-Container'>
@@ -41,21 +38,36 @@ const LibrarySearch = () => {
         <div className='Library-SearchBar-Wrapper'>
           <label htmlFor='search-bar'>
             <input
-              type='search'
-              name='search-bar'
+              type="text"
+              name="search-bar"
               className='Library-Search-Input'
               placeholder='Search Library Here...'
               value={searchInput}
-              // When the user types the search input state is set to the value
               onChange={(e) => setSearchInput(e.target.value)}
             />
           </label>
+          <div className='Library-Genre-Select'>
+            <select
+              value={filterParam}
+              onChange={(e) => {setFitlerParam(e.target.value)}}
+              className='genre-select'
+              aria-label='Filter Books by Genre'
+            >
+              <option value="">Filter By Genre</option>
+              <option value="Java">Java</option>
+              <option value="Devops">DevOps</option>
+              <option value="Management">Mangement</option>
+              <option value="Python">Python</option>
+              <option value="Buisness">Buisness</option>
+            </select>
+            <span className='focus'></span>
+          </div>
         </div>
 
         <div className='Library-SearchResults-Wrapper'>
           <ul className='Library-BookList'>
-            {fakebooks.map((book) => (
-              <li className='Library-Item' key={book.id}>
+            {search(dataMock).map((book) => (
+              <li className='Library-Item' key={book.name}>
                 <article className='Library-Book'>
                   <div className='Library-Book-Cover'>
                     <img className='Library-Book-Cover-Image' src={BookCover} alt={book.name} />
