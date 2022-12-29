25e51f88be38a177df3c88f417be760c241d3720
kya-edmeade <kya.edmeade@ecs.co.uk>
2022-12-29 20:27:49 +0000

    Case Studies header carousel

---
+++ src/components/carousel/CaseStudiesCarousel.jsx
@@ -0,0 +1,48 @@
+import '../carousel/MainCarousel.css';
+import { useEffect, useState } from 'react';
+import CarouselCards from './shared-components/CarouselCards';
+import CarouselTextbox from './shared-components/CarouselTextbox';
+
+const CaseStudyCarousel = ({ images }) => {
+  const [current, setCurrent] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+
+  let timeOut = null;
+
+  const paginationDots = images.map((img, index) => <div className={index === current ? 'pagination_dot pagination_dot-active' : 'pagination_dot'} onClick={() => setCurrent(index) && clearTimeout(timeOut)}></div>);
+
+  useEffect(
+    () => {
+      timeOut =
+        autoPlay &&
+        setTimeout(() => {
+          slideRight();
+        }, 5000);
+    }
+    //timer function- disables error as timer doesn't need to be stored
+  );
+
+  const slideRight = () => {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }; // slide right and current checks what image it is on - it will go back to one if it reaches the end
+
+  const handleMouseEnter = () => {
+    setAutoPlay(false);
+    clearTimeout(timeOut);
+  };
+
+  return (
+    <div className='carousel-background'>
+      <div className='carousel' onMouseEnter={handleMouseEnter} onMouseLeave={() => setAutoPlay(true)}>
+        <div className='carousel_wrapper'>
+          <CarouselCards images={images} current={current} paginationDots={paginationDots} />
+        </div>
+        <div className='carousel_wrapper_second'>
+          <CarouselTextbox images={images} current={current} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaseStudyCarousel;
---
+++ src/pages/CaseStudies.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CaseStudyCarousel from '../components/carousel/CaseStudiesCarousel';
+import ContactComponent from '../components/contact-component/ContactComponent';
+
+const CaseStudies = () => {
+  return 
+  <>
+  <CaseStudyCarousel/>
+  <ContactComponent feedbackType='case-study' />;
+  </>
+  
+ 
+};
+
+export default CaseStudies;
