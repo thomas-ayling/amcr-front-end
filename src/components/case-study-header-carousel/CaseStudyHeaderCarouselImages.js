1e9afa2729d8010cd5b9da18a8d053e7687b434b
kya-edmeade <kya.edmeade@ecs.co.uk>
2023-01-04 10:09:55 +0000

    Header carousel no link

---
+++ src/components/case-study-header-carousel/CaseStudyHeaderCarouselImages.js
@@ -0,0 +1,33 @@
+import Slide1 from '../../resources/case-studies-header-carousel-images/jonathan-arbely-rG4Gq4MCmgU-unsplash.jpg';
+import Slide2 from '../../resources/case-studies-header-carousel-images/polina-kuzovkova-r9v4Sc0pHMU-unsplash.jpg';
+import Slide3 from '../../resources/case-studies-header-carousel-images/sidekix-media-OqZKI15yBnM-unsplash.jpg';
+
+
+const CaseStudyHeaderCarouselImages =[
+    {   
+        image: Slide1,
+        title: "Global Logic, Manchester",
+        page: "case-study-1",
+        link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
+        overview: "Bitcoin is the future" , 
+    },
+    {   
+        image: Slide2,
+        title: "Global Logic, Manchester",
+        page: "case-study-2",
+        link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
+        overview: "19th Floor, Manchester One, 53 Portland Street, Manchester, M1 3LD" , 
+       
+    },
+    {   
+        image: Slide3,
+        title: "Global Logic, Manchester",
+        page: "case-study-3",
+        link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
+        overview: "19th Floor, Manchester One, 53 Portland Street, Manchester, M1 3LD" , 
+    },
+    
+
+]
+
+export default CaseStudyHeaderCarouselImages;
--- src/pages/CaseStudies.jsx
+++ src/pages/CaseStudies.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import CaseStudyHeaderCarouselImages from '../components/case-study-header-carousel/CaseStudyHeaderCarouselImages';
 import ContactComponent from '../components/contact-component/ContactComponent';
+import MainCarousel from '../components/carousel/MainCarousel';
+import CaseStudiesHeaderCarousel from '../components/case-study-header-carousel/HeaderCarousel';
 
 const CaseStudies = () => {
-  return <ContactComponent feedbackType='case-study' />;
+  return (
+    <>
+    <div >
+    <div className='page' ><CaseStudiesHeaderCarousel images={CaseStudyHeaderCarouselImages} /></div>;
+      
+      <div><ContactComponent feedbackType='case-study' />;</div>
+      </div>
+    </>
+  );
 };
 
 export default CaseStudies;
