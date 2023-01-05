048d8a34811174a166cfc50c698d51d44ed151f0
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-05 15:31:32 +0000

    created toastify component

---
+++ src/components/error-messages-component/ErrorMessage.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+const ErrorMessage = () => {
+
+    return(
+        <ToastContainer/>
+    )
+}
+
+export default ErrorMessage;
