4ee8151b2e77277d8d38006ba2932e5c6de7c9b1
kya-edmeade <kya.edmeade@ecs.co.uk>
2023-01-06 12:22:42 +0000

    Layout update

--- client/src/pages/ConfigurationPage.jsx
+++ client/src/pages/ConfigurationPage.jsx
@@ -1,7 +1,7 @@
 import Grid from '../components/Layout';
 import Mount from '../components/Mounted'
-import '/node_modules/react-grid-layout/css/styles.css';
-import '/node_modules/react-resizable/css/styles.css';
+import '../../../node_modules/react-grid-layout/css/styles.css';
+import '../../../node_modules/react-resizable/css/styles.css';
 
 const Configuration = () => {
     
