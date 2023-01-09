06fd587fc3a6a4364691f688c6a7f6f5858c7d0e
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-09 09:25:51 +0000

    implementing context

--- src/components/error-messages-component/ErrorMessage.jsx
+++ src/components/error-messages-component/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import react, {useState, createContext, useContext} from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
-const ErrorMessage = () => {
+const ErrorMessage = ({ message, runMessage }) => {
     const [message, setMessage] = useState("");
     const runMessage = () => {
         toast(message);
