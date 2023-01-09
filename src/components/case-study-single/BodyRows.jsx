a3c5b194325f73ad1ecf77e000eb14bf8b5835d7
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-09 15:34:34 +0000

    Cleaned up code warnings

--- src/components/case-study-single/BodyRows.jsx
+++ src/components/case-study-single/BodyRows.jsx
@@ -4,7 +4,7 @@ import MarkdownComponent from '../markdown-component/MarkdownComponent';
 const BodyRows = ({ data }) => {
    return data.map((item, index) => (
     <div className={`cssp-body-row cssp-${index % 2 === 0 ? 'regular' : 'reversed'}-row`} key={index}>
-      <img className='cssp-body-img' src={item.imageLink} />
+      <img className='cssp-body-img' src={item.imageLink} alt={item.title} />
       <MarkdownComponent markdownText={item.markdownText} classNames='cssp-body-text' />
     </div>
   ));
