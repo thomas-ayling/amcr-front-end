4583e0548ae8c08366fb7bbf45158e1bc935bb20
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-09 14:26:55 +0000

    trying to save the diagram component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import MainRoutes from './pages/MainRoutes';
 import Footer from './components/footer/Footer';
 import React from 'react';
 import 'react-bootstrap';
+import Diagram from'./components/diagram-component/Diagram';
 
 function App() {
   return (
-      <Router>
-        <NavBar />
-        <MainRoutes />
-        <Footer />
-      </Router>
+      // <Router>
+      //   <NavBar />
+      //   <MainRoutes />
+      //   <Footer />
+      // </Router>
+      <Diagram/>
   );
 }
 
