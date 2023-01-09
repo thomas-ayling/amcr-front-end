16cd0f5ec675e160441043cf78379d2c3e2064a4
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-09 14:39:42 +0000

    More carousel cleanup

--- src/components/main-carousel/shared-carousel-components/CarouselTitles.jsx
+++ src/components/main-carousel/shared-carousel-components/CarouselTitles.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './CarouselTitles.css';
 
 const CarouselTitles = ({ slides, current }) => {
-  return slides.map((slide, index) => <div className={index === current ? 'carousel-title active-carousel-title' : 'carousel-title'}>{slide.title}</div>);
+  return slides.map((slide, index) => <div key={index} className={index === current ? 'carousel-title active-carousel-title' : 'carousel-title'}>{slide.title}</div>);
 };
 
 export default CarouselTitles;
--- src/components/main-carousel/shared-carousel-components/SlideDots.jsx
+++ src/components/main-carousel/shared-carousel-components/SlideDots.jsx
@@ -4,7 +4,7 @@ const SlideDots = ({ slides, current, setCurrent }) => {
   return (
     <div className='slide-dots'>
       {slides.map((_, index) => (
-        <div className={index === current ? 'pagination-dot pagination-dot-active' : 'pagination-dot'} onClick={() => setCurrent(index)}></div>
+        <div key={index} className={index === current ? 'pagination-dot pagination-dot-active' : 'pagination-dot'} onClick={() => setCurrent(index)}></div>
       ))}
     </div>
   );
