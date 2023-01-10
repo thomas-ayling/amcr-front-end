8de6593e6b51b0678a06602950299be6697572ae
kya-edmeade <kya.edmeade@ecs.co.uk>
2023-01-10 10:14:31 +0000

    Changed case study header carousel titles

--- src/service/CaseStudyHeaderCarouselMockService.js
+++ src/service/CaseStudyHeaderCarouselMockService.js
@@ -6,7 +6,7 @@ import Slide3 from '../resources/case-studies-header-carousel-images/sidekix-med
 const CaseStudyHeaderCarouselImages =[
     {   id: 1,
         image: Slide1,
-        title: "K-dawg",
+        title: "Case study 1",
         page: "case-study-1",
         link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
         overview: "Bitcoin is the future" , 
@@ -14,7 +14,7 @@ const CaseStudyHeaderCarouselImages =[
     {   
         id: 2,
         image: Slide2,
-        title: "Ruby Rubester",
+        title: "Case Study 2",
         page: "case-study-2",
         link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
         overview: "19th Floor, Manchester One, 53 Portland Street, Manchester, M1 3LD" , 
@@ -23,7 +23,7 @@ const CaseStudyHeaderCarouselImages =[
     {   
         id: 3,
         image: Slide3,
-        title: "EDDY Edmeade",
+        title: "Case Study 3",
         page: "case-study-3",
         link: "https://www.google.com/maps/place/19th+Floor,+Manchester+One,+53+Portland+St,+Manchester+M1+3LD,+UK/@53.4781733,-2.2381423,17z/data=!3m1!4b1!4m5!3m4!1s0x487bb1bff8a01245:0x6c4635b9b3558dce!8m2!3d53.4781733!4d-2.2381423?sa=X&ved=2ahUKEwi397-m9Ov7AhX91zgGHZXUD8QQ8gF6BAgIEAE&coh=164777&entry=tt&shorturl=1",
         overview: "19th Floor, Manchester One, 53 Portland Street, Manchester, M1 3LD" , 
