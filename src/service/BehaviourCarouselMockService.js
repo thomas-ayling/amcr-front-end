d4779cbb20d305845c2a9f8908448c16497c6fbd
kya-edmeade <kya.edmeade@ecs.co.uk>
2023-01-12 15:15:21 +0000

    behaviour service fixed

--- src/service/BehaviourCarouselMockService.js
+++ src/service/BehaviourCarouselMockService.js
@@ -66,38 +66,38 @@ const InnovationDescription = [
   <br />,
   <br />,
   '\u2022 Experimentation',
-=======
+
   '\n\u2022 We do what we say we will do\n\n',
   '\u2022 We tell the truth\n\n',
   "\u2022 We don't make empty promises\n\n",
   '\u2022 We align to business value\n\n',
 ];
 
-const OpennessDescription = [
-  '\n\u2022 We can always explain our decisions\n\n',
-  '\u2022 We accept when others do things better\n\n',
-  '\u2022 We acknowledge failure\n\n',
-  '\u2022 We share our knowledge\n\n',
-];
+// const OpennessDescription = [
+//   '\n\u2022 We can always explain our decisions\n\n',
+//   '\u2022 We accept when others do things better\n\n',
+//   '\u2022 We acknowledge failure\n\n',
+//   '\u2022 We share our knowledge\n\n',
+// ];
 
-const TeamworkDescription = [
-  '\n\u2022 We work to build an inclusive environment\n\n',
-  '\u2022 Collaboration is focussed\n\n',
-  '\u2022 We emphasise the team over the individual\n\n',
-  '\u2022 Colleague progression\n\n',
-  '\u2022 We are courageous\n\n',
-  '\u2022 A respectful environment\n\n',
-];
+// const TeamworkDescription = [
+//   '\n\u2022 We work to build an inclusive environment\n\n',
+//   '\u2022 Collaboration is focussed\n\n',
+//   '\u2022 We emphasise the team over the individual\n\n',
+//   '\u2022 Colleague progression\n\n',
+//   '\u2022 We are courageous\n\n',
+//   '\u2022 A respectful environment\n\n',
+// ];
 
-const InnovationDescription = [
-  '\n\u2022 Efficiency\n\n',
-  '\u2022 Operational design\n\n',
-  '\u2022 Build and develop talent\n\n',
-  '\u2022 GlobalLogic knowledge\n\n',
-  '\u2022 Research\n\n',
-  '\u2022 Experimentation\n\n',
+// const InnovationDescription = [
+//   '\n\u2022 Efficiency\n\n',
+//   '\u2022 Operational design\n\n',
+//   '\u2022 Build and develop talent\n\n',
+//   '\u2022 GlobalLogic knowledge\n\n',
+//   '\u2022 Research\n\n',
+//   '\u2022 Experimentation\n\n',
 
-];
+// ];
 
 const behaviourSlides = [
   {
