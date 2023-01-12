6e01ccc0673fbc5e4a1e1f3e452c37981bd471a3
Kya-Edmeade <119429408+Kya-Edmeade@users.noreply.github.com>
2023-01-12 16:40:04 +0000

    merge conflicts

--- src/components/main-carousel/shared-carousel-components/CarouselTitles.jsx
+++ src/components/main-carousel/shared-carousel-components/CarouselTitles.jsx
@@ -3,7 +3,7 @@ import './CarouselTitles.css';
 
 
 const CarouselTitles = ({ slides, current, classNames }) => {
-  return slides.map((slide, index) => <div key={index} className={`carousel-title ${index === current && 'active-carousel-title'} ${classNames}`}>{slide.title}</div>);
+    return slides.map((slide, index) => <div key={index} className={`carousel-title ${index === current && 'active-carousel-title'} ${classNames}`}>{slide.title}</div>);}
 
 
 
