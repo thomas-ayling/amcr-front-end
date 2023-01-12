4bb5b0678d24b2c73096a1976f5b478bd67b2005
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-12 14:10:50 +0000

    Updated input checking for the user inputs

--- src/components/contact-component/input-components/input-component-types/CaseStudyInputs.jsx
+++ src/components/contact-component/input-components/input-component-types/CaseStudyInputs.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AttachmentInput from '../shared-input-components/AttachmentInput';
 import FeedbackBodyTextarea from '../shared-input-components/FeedbackBodyTextarea';
 import UserInfoInputs from '../shared-input-components/UserInfoInputs';
 import SubmitButton from '../shared-input-components/SubmitButton';
 
 const CaseStudyInputs = ({ firstName, setFirstName, lastName, setLastName, emailAddress, setEmailAddress, feedbackBody, setFeedbackBody, attachment, setAttachment, handleSubmit, awaitingResponse, submitStatus }) => {
-  return (
+  return ( 
     <div className='contact-form-container'>
       <form onSubmit={handleSubmit} className='casestudy-form contact-form'>
         <UserInfoInputs setFirstName={setFirstName} setLastName={setLastName} setEmailAddress={setEmailAddress} firstName={firstName} lastName={lastName} emailAddress={emailAddress} mandatory={true} />
