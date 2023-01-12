c7364670474fb1848b2a6b89ec80d6376878fc4e
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-12 12:17:35 +0000

    imported both css files

--- src/components/toast-notification/ToastNotification.jsx
+++ src/components/toast-notification/ToastNotification.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import './ToastNotification.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const runToastNotification = (message, toastType) => {
   if (toastType === 'success') return toast.success(message, { position: 'top-center', autoClose: 5000 });
