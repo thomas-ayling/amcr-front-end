0bad4bc97aedc25765538c5117c4edd367c7d3d5
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-01-20 11:51:49 +0000

    altered service

--- src/service/LibraryHeader.js
+++ src/service/LibraryHeader.js
@@ -3,9 +3,8 @@ import Image1 from "../assets/images/library/header/library-header.webp";
 const LibraryHeader = [
   {
     image: Image1,
-    title: "Library",
-    description: "We are the national library of the United Kingdom and give access to the worlds most comprehensive research collection. We provide information services to academic, business, research and scientific communities.",
-  },
+    title: "Library"
+    },
 ];
 
 export default LibraryHeader;
