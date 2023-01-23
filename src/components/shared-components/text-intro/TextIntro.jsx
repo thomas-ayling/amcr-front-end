e57fc9a5c848ad8d83d3f34bdf62a954dda7417a
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-01-23 16:04:22 +0000

    Implemented the Markdown Component

--- src/components/shared-components/text-intro/TextIntro.jsx
+++ src/components/shared-components/text-intro/TextIntro.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TextIntro.css';
 import { StyledHr } from '../../../styles/styles';
+import MarkdownComponent from '../../markdown-component/MarkdownComponent';
 
 
 const TextIntro = ({content}) => {
@@ -9,7 +10,9 @@ const TextIntro = ({content}) => {
         <div className='textintro-wrapper'>
           <h1 className='textintro-title'>{content.title}</h1>
           <StyledHr />
-          <h2 className='textintro-description'>{content.description}</h2>
+          <h2 className='textintro-description'>
+            <MarkdownComponent markdownText={content.description}/>
+          </h2>
         </div>
       </div>
     )
