23f8465e4f4b1cc3661821343fb87a2e08ef9e1b
Kya-Edmeade <119429408+Kya-Edmeade@users.noreply.github.com>
2023-01-24 10:03:29 +0000

    Wiki header started

--- src/pages/Wiki.jsx
+++ src/pages/Wiki.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MainCarousel from '../components/carousels/main-carousel/MainCarousel';
 import ContactComponent from '../components/contact-component/ContactComponent';
 import Diagram from '../components/diagram-component/Diagram';
-
+import LibraryHeader from '../service/LibraryHeader';
 const Wiki = () => {
   return (
     <>
+      <MainCarousel type='header-single' slides={LibraryHeader} />
       <Diagram adminEnabled={false} />
       <ContactComponent feedbackType={'improvement'} />
     </>
---
+++ src/service/WikiHeaderMockService.js
@@ -0,0 +1,6 @@
+const wikiHeader = [
+  {
+    image: Image1,
+    title: 'Welcome to the Wiki page',
+  },
+];
