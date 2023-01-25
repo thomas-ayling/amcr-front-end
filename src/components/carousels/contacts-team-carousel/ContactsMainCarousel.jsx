e27330ef02a9e7f15f605ab1248961434f4041f5
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-25 08:51:06 +0000

    connected carousel to backend

--- src/components/carousels/contacts-team-carousel/ContactsMainCarousel.jsx
+++ src/components/carousels/contacts-team-carousel/ContactsMainCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Slider from 'react-slick';
 
 import './ContactsMainCarousel.css';
@@ -6,12 +6,27 @@ import './slick-css-files/slick.css';
 import './slick-css-files/slick-theme.css';
 
 import ContactsCarouselCard from './ContactsCarouselCard';
-import contactsTeamData from '../../../service/ContactsMainCarouselMockService';
+import LoaderGif from '../../shared-components/LoaderGif';
 import { StyledHr } from '../../../styles/styles';
 import { HiOutlineArrowNarrowLeft, HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import { getSpotlit } from '../../../service/ContactsCarouselService';
+import { runToastNotification } from '../../toast-notification/ToastNotification';
 
 const ContactsMainCarousel = () => {
   const slider = useRef();
+
+  const [carouselData, setCarouselData] = useState();
+  const [requestStatus, setRequestStatus] = useState('');
+
+  useEffect(() => {
+    getSpotlit(setCarouselData, setRequestStatus);
+  }, []);
+
+  useEffect(() => {
+    if (requestStatus === 'error-404') runToastNotification(`Carousel data not found!`, 'error');
+    else if (requestStatus === 'other-error') runToastNotification('Could not load carousel data!', 'error');
+  }, [requestStatus]);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -42,7 +57,7 @@ const ContactsMainCarousel = () => {
         },
       },
       {
-        breakpoint: 500,
+        breakpoint: 580,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -51,24 +66,28 @@ const ContactsMainCarousel = () => {
     ],
   };
 
-  return (
-    <div className='contacts-carousel'>
-      <Slider ref={slider} {...settings}>
-        {contactsTeamData.map((elem, i) => (
-          <ContactsCarouselCard index={i} image={elem.image} name={elem.name} title={elem.title} description={elem.description} />
-        ))}
-      </Slider>
-      <div className='contacts-carousel-arrows'>
-        <div className='contacts-carousel-arrow-left'>
-          <HiOutlineArrowNarrowLeft size={90} onClick={() => slider.current.slickPrev()} />
+  if (requestStatus.includes('success'))
+    return (
+      <div className='contacts-carousel'>
+        <Slider ref={slider} {...settings}>
+          {carouselData.map((elem, i) => (
+            <ContactsCarouselCard index={i} image={elem.imageLink} name={elem.fullName} title={elem.title} description={elem.description} />
+          ))}
+        </Slider>
+        <div className='contacts-carousel-arrows'>
+          <div className='contacts-carousel-arrow-left'>
+            <HiOutlineArrowNarrowLeft size={90} onClick={() => slider.current.slickPrev()} />
+          </div>
+          <div className='contacts-carousel-arrow-right'>
+            <HiOutlineArrowNarrowRight size={90} onClick={() => slider.current.slickNext()} />
+          </div>
         </div>
-        <div className='contacts-carousel-arrow-right'>
-          <HiOutlineArrowNarrowRight size={90} onClick={() => slider.current.slickNext()} />
-        </div>
+        <StyledHr />
       </div>
-      <StyledHr />
-    </div>
-  );
+    );
+  if (requestStatus.includes('success')) {
+    return <LoaderGif />;
+  }
 };
 
 export default ContactsMainCarousel;
