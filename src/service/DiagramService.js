628b16323cbc907d545fe6b177d545c055a1c3a6
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-25 09:06:56 +0000

    fixed diagram titles bug

--- src/service/DiagramService.js
+++ src/service/DiagramService.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { runToastNotification } from '../components/toast-notification/ToastNotification.jsx';
 
-// const baseUrl = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001';
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'http://ec-acad-elb-fe-2f7bfb1526a617ef.elb.eu-west-2.amazonaws.com:3001';
+// const baseUrl = 'http://localhost:3001';
 const id = '99ffb583-6dc1-4e5c-8448-c2c3d8983b28';
 
 const get = (setNodeArray, setRequestStatus) => {
