8a514ad55336f2bfeaf3bb4d2e5a555bedac26ac
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-01-26 12:38:29 +0000

    added wireframe for admin panel

--- src/components/carousels/contacts-team-carousel/admin-functionality/ContactsAdminPanel.jsx
+++ src/components/carousels/contacts-team-carousel/admin-functionality/ContactsAdminPanel.jsx
@@ -2,18 +2,36 @@ import React from 'react';
 
 const ContactsAdminPanel = ({ carouselData }) => {
   return (
-    <>
-      <label className='contacts-carousel-admin-dropdown-label' htmlFor='contacts-carousel-admin-dropdown-select'>
-        Select carousel component to modify:
-      </label>
-      <select id='contacts-carousel-admin-dropdown-select' className='contacts-carousel-admin-dropdown-select' name='name' value='value'>
-        {carouselData.map((elem, i) => (
-          <option value={elem.fullName} key={i}>
-            {elem.fullName} ({elem.id})
-          </option>
-        ))}
-      </select>
-    </>
+    <div className='contacts-carousel-admin-panel'>
+      <div className='contacts-carousel-admin-dropdown'>
+        <label className='contacts-carousel-admin-dropdown-label' htmlFor='contacts-carousel-admin-dropdown-select'>
+          Select carousel component to modify:
+        </label>
+        <select id='contacts-carousel-admin-dropdown-select' className='contacts-carousel-admin-dropdown-select' name='name' value='value'>
+          {carouselData.map((elem, i) => (
+            <option value={elem.fullName} key={i}>
+              {elem.fullName} ({elem.id})
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className='contacts-carousel-admin-buttons'>
+        <button>Add new</button>
+        <button>Delete selected</button>
+        <button>Confirm</button>
+        <button>Cancel</button>
+      </div>
+      <div className='contacts-carousel-admin-inputs'>
+        <div>
+          <label for='contacts-carousel-admin-input-spotlight'>Spotlight</label>
+          <input type='checkbox' id='contacts-carousel-admin-input-spotlight' className='contacts-carousel-admin-input-spotlight' />
+        </div>
+        <textarea className='contacts-carousel-admin-input-name' placeholder='full name' />
+        <textarea className='contacts-carousel-admin-input-title' placeholder='job title' />
+        <textarea className='contacts-carousel-admin-input-description' placeholder='description' />
+        <p>*Attachment component to go here*</p>
+      </div>
+    </div>
   );
 };
 
