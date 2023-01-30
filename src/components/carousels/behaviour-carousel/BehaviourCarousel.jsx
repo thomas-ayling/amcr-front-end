5f88d41026e28a3033b0b6a35500e72f71532303
S1sO0 <119429167+S1sO0@users.noreply.github.com>
2023-01-30 14:27:06 +0000

    Committing some changes to css, they are not final.

--- src/components/carousels/behaviour-carousel/BehaviourCarousel.jsx
+++ src/components/carousels/behaviour-carousel/BehaviourCarousel.jsx
@@ -27,18 +27,20 @@ const BodyCarousel = () => {
 
   return (
     <div className='behaviour-body-carousel-wrapper'>
-      <h1 className='behaviour-body-carousel-page-title'>GlobalLogic UK&I Behaviours</h1>
-      <div className='behaviour-carousel-container'>
-        <div className='behaviour-carousel-content'>
-          <BehaviourCarouselDescription current={current} windowWidth={windowWidth} dataArray={BehaviourImages} />
+      <div className='behaviour-carousel-inner-wrapper'>
+        <h1 className='behaviour-body-carousel-page-title'>GlobalLogic UK&I Behaviours</h1>
+        <div className='behaviour-carousel-container'>
+          <div className='behaviour-carousel-content'>
+            <BehaviourCarouselDescription current={current} windowWidth={windowWidth} dataArray={BehaviourImages} />
+          </div>
         </div>
-      </div>
-      <div className='behaviour-carousel-arrows'>
-        <div className='behaviour-carousel-arrow-left' onClick={clickLeft}>
-          <HiOutlineArrowNarrowLeft size={75} />
-        </div>
-        <div className='behaviour-carousel-arrow-right' onClick={clickRight}>
-          <HiOutlineArrowNarrowRight size={75} />
+        <div className='behaviour-carousel-arrows'>
+          <div className='behaviour-carousel-arrow-left' onClick={clickLeft}>
+            <HiOutlineArrowNarrowLeft size={75} />
+          </div>
+          <div className='behaviour-carousel-arrow-right' onClick={clickRight}>
+            <HiOutlineArrowNarrowRight size={75} />
+          </div>
         </div>
       </div>
     </div>
