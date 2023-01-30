da1b47092f372cb11ab3dc56e90fb786a3b78a6b
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-01-30 17:41:33 +0000

    Potential Carousel Changes

--- src/components/carousels/behaviour-carousel/BehaviourCarousel.jsx
+++ src/components/carousels/behaviour-carousel/BehaviourCarousel.jsx
@@ -2,6 +2,7 @@ import './BehaviourCarousel.css';
 import BehaviourImages from '../../../service/BehaviourCarouselMockService';
 import { useState } from 'react';
 import BehaviourCarouselDescription from './behaviour-carousel-components/BehaviourCarouselDescription';
+import { StyledHr } from '../../../styles/styles';
 
 import { HiOutlineArrowNarrowLeft, HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
@@ -27,18 +28,21 @@ const BodyCarousel = () => {
 
   return (
     <div className='behaviour-body-carousel-wrapper'>
-      <h1 className='behaviour-body-carousel-page-title'>GlobalLogic UK&I Behaviours</h1>
-      <div className='behaviour-carousel-container'>
-        <div className='behaviour-carousel-content'>
-          <BehaviourCarouselDescription current={current} windowWidth={windowWidth} dataArray={BehaviourImages} />
+      <div className='behaviour-body-carousel-border'>
+        <h1 className='behaviour-body-carousel-page-title'>GlobalLogic UK&I Behaviours</h1>
+        <StyledHr />
+        <div className='behaviour-carousel-container'>
+          <div className='behaviour-carousel-content'>
+            <BehaviourCarouselDescription current={current} windowWidth={windowWidth} dataArray={BehaviourImages} />
+          </div>
         </div>
-      </div>
-      <div className='behaviour-carousel-arrows'>
-        <div className='behaviour-carousel-arrow-left' onClick={clickLeft}>
-          <HiOutlineArrowNarrowLeft size={75} />
-        </div>
-        <div className='behaviour-carousel-arrow-right' onClick={clickRight}>
-          <HiOutlineArrowNarrowRight size={75} />
+        <div className='behaviour-carousel-arrows'>
+          <div className='behaviour-carousel-arrow-left' onClick={clickLeft}>
+            <HiOutlineArrowNarrowLeft size={75} />
+          </div>
+          <div className='behaviour-carousel-arrow-right' onClick={clickRight}>
+            <HiOutlineArrowNarrowRight size={75} />
+          </div>
         </div>
       </div>
     </div>
