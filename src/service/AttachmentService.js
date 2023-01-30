5a0c53b5522eff4af6471c0b6261a4c9c596a18e
S1sO0 <119429167+S1sO0@users.noreply.github.com>
2023-01-30 14:05:45 +0000

    Attachment component IP address bug fixed.

--- src/service/AttachmentService.js
+++ src/service/AttachmentService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Crc32c } from '@aws-crypto/crc32c';
 
-const baseUrl = 'http://10.61.60.249:3001/attachment/';
+const baseUrl = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/attachment/';
 
 function upload(attachment, setResponseStatus, setDownloadUri) {
   const reader = new FileReader();
