41da5d08f608262cc79d06fdf5a43113c34dca1f
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-01-30 15:51:16 +0000

    continued development

--- src/components/case-study-single/EditDownloadLinks.jsx
+++ src/components/case-study-single/EditDownloadLinks.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { useState, useEffect, React } from 'react';
+import { uploadAttachment } from '../../service/AttachmentService';
+import { runToastNotification } from '../toast-notification/ToastNotification';
 
 const DownloadLinks = ({ attachmentMetadata, attachmentLinks, setAttachmentLinks }) => {
-    const handleChangeFile = (file, index) => {
-        
+  const [responseStatus, setResponseStatus] = useState('idle');
+  const [downloadUri, setDownloadUri] = useState();
 
-        const newAttachmentLinks = [...attachmentLinks];
-        newAttachmentLinks[index] = 
-    }
+  useEffect(() => {
+    if (responseStatus === 'success') runToastNotification('Attachment uploaded successfuly', 'success');
+    if (responseStatus === 'error') runToastNotification('There was an error uploading this attachment', 'error');
+    const newAttachmentLinks = [...attachmentLinks];
+    newAttachmentLinks[index] = downloadUri;
+    setAttachmentLinks(newAttachmentLinks);
+  }, [downloadUri, responseStatus]);
+
+  const handleChangeFile = (attachment, index) => {
+    uploadAttachment(attachment, setResponseStatus, setDownloadUri);
+  };
 
   if (attachmentMetadata) {
     const downloadFile = (link) => {
