16eb11aeca0839af47f209b7ce408249ef270c19
Thomas Ayling <119429101+thomas-ayling@users.noreply.github.com>
2023-01-31 13:55:17 +0000

    Update CaseStudySingleService.js

--- src/service/CaseStudySingleService.js
+++ src/service/CaseStudySingleService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getMetadata } from './AttachmentMetadataService.js';
-const baseUrl = 'http://localhost:3001/case-study';
-// const baseUrl = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/case-study';
+// const baseUrl = 'http://localhost:3001/case-study';
+const baseUrl = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/case-study';
 
 const get = (id, setPageData, setRequestStatus, setPageLoaded, setAttachmentMetadata) => {
   axios
