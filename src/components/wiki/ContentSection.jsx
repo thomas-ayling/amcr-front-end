442ec6c8b28035b14a79def9a5c63e8392ba26c6
MihneaDinuGL <mihnea.dinu@ecs.co.uk>
2023-02-02 11:33:40 +0000

    a bit more refactoring

--- src/components/wiki/ContentSection.jsx
+++ src/components/wiki/ContentSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import ContentSectionElement from './ContentSectionElement';
 import { Link } from 'react-router-dom';
-import { FaWikipediaW } from 'react-icons/fa';
+import { FaWikipediaW, FaPlus } from 'react-icons/fa';
 import './contentSection.css';
 
 const ContentSection = () => {
@@ -8,33 +9,10 @@ const ContentSection = () => {
     <div className='content-section-container'>
       <div className='content-section-wrapper'>
         <div className='content-section-icons-container'>
-          <div className='content-section-span'>
-            <Link to={'/engineering-excellence'}>
-              <FaWikipediaW size={46} className='content-section-icons' />
-            </Link>
-            <span className='content-section-space-between-icon-and-text'>Engineering Excellence</span>
-          </div>
-
-          <div className='content-section-span'>
-            <Link to={'/employee-journey'}>
-              <FaWikipediaW size={46} className='content-section-icons' />
-            </Link>
-            <span className='content-section-space-between-icon-and-text'>Employee Journey</span>
-          </div>
-
-          <div className='content-section-span'>
-            <Link to={'/charter-and-okrs'}>
-              <FaWikipediaW size={46} className='content-section-icons' />
-            </Link>
-            <span className='content-section-space-between-icon-and-text'>Charter & OKRs</span>
-          </div>
-
-          <div className='content-section-span'>
-            <Link to={'/lab-science-decks'}>
-              <FaWikipediaW size={46} className='content-section-icons' />
-            </Link>
-            <span className='content-section-space-between-icon-and-text'>Lab Science Decks</span>
-          </div>
+          <ContentSectionElement linkRef={'/engineering-excellence'} title={'Engineering Excellence'} />
+          <ContentSectionElement linkRef={'/employee-journey'} title={'Employee Journey'} />
+          <ContentSectionElement linkRef={'/charter-and-okrs'} title={'Charter & OKRs'} />
+          <ContentSectionElement linkRef={'/lab-science-decks'} title={'Lab Science Decks'} />
         </div>
       </div>
     </div>
