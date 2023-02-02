5a6cb331b54fac231a73043c6c17ce55adb02707
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-02-02 10:54:17 +0000

    stashed - incomplete

--- src/components/shared-components/text-intro/TextIntro.jsx
+++ src/components/shared-components/text-intro/TextIntro.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import axios from 'axios';
 import './TextIntro.css';
 import { StyledHr } from '../../../styles/styles';
 import MarkdownComponent from '../../markdown-component/MarkdownComponent';
-import { getTextIntro } from '../../../service/TextIntroService';
+// const baseUrl = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/feedback';
 
 const TextIntro = (location) => {
-  const [textIntroData, setTextIntroData] = useState('');
+  const [textIntroData, setTextIntroData] = useState([]);
+  const baseUrl = 'http://localhost:3001/text-intro/location';
+  const headers = { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*", "Access-Control-Allow-Credentials": "true" };
+
   
   useEffect(() => {
-    setTextIntroData(getTextIntro(location));
+    axios
+      .get(`${baseUrl}/${location}`, { headers: headers })
+      .then((result) => {
+        console.log(result);
+        setTextIntroData(result.data);
+        console.log(textIntroData);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
 
     return (
+      <div>
+        {textIntroData.map((intro) => (        
         <div className='textintro-container'>
-        <div className='textintro-wrapper'>
-          <h1 className='textintro-title'>{textIntroData.title}</h1>
-          <StyledHr />
-          <h2 className='textintro-description'>
-            <MarkdownComponent markdownText={textIntroData.description}/>
-          </h2>
+          <div className='textintro-wrapper'>
+            <h1 className='textintro-title'>{intro.title}</h1>
+            <StyledHr />
+            <h2 className='textintro-description'>
+              <MarkdownComponent markdownText={intro.description}/>
+            </h2>
+          </div>
         </div>
-      </div>
+    ))}
+    </div>
     )
 }
 
