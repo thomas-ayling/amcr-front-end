d4cfb510642d59469b78967babbae408bbc2e664
CSCairney-GL <119429189+CSCairney-GL@users.noreply.github.com>
2023-02-03 16:40:56 +0000

    Fixed Nav Links to update to the top of the page.

--- src/components/shared-components/navbar/NavBar.jsx
+++ src/components/shared-components/navbar/NavBar.jsx
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Logo from '../../../assets/logo/GLLogo.png';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import './NavBar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../../assets/logo/GLLogo.png";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import "./NavBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
+  const [expand, setExpanded] = useState(false);
+  const [navColour, setNavbar] = useState(false);
   const location = useLocation();
+
+  const pageReload = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  };
 
   function scrollHandler() {
     if (window.scrollY >= 20) {
-      updateNavbar(true);
+      setNavbar(true);
     } else {
-      updateNavbar(false);
+      setNavbar(false);
     }
   }
 
-  window.addEventListener('scroll', scrollHandler);
+  const changeView = (expandedVal) => {
+    setExpanded(expandedVal);
+    pageReload();
+  };
+
+  window.addEventListener("scroll", scrollHandler);
 
   return (
-    <Navbar expanded={expand} fixed='top' expand='md' className={navColour ? 'navbar-sticky' : 'navbar'}>
+    <Navbar expanded={expand} fixed='top' expand='md' className={navColour ? "navbar-sticky" : "navbar"}>
       <Navbar.Brand href='/' className='d-flex'>
         <img src={Logo} className='img-fluid logo-menu' alt='brand' />
       </Navbar.Brand>
-      <Navbar.Toggle
-        aria-controls='responsive-navbar-nav'
-        onClick={() => {
-          updateExpanded(expand ? false : 'expanded');
-        }}
-      >
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' onClick={() => setExpanded(expand ? false : "expanded")}>
         <span></span>
         <span></span>
         <span></span>
@@ -40,25 +47,25 @@ const NavBar = () => {
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='ms-auto' defaultActiveKey='#home'>
           <Nav.Item>
-            <Nav.Link as={Link} to='/case-studies' active={location.pathname === '/case-studies'} onClick={() => updateExpanded(false)}>
+            <Nav.Link as={Link} to='/case-studies' active={location.pathname === "/case-studies"} onClick={() => changeView(false)}>
               Case Studies
             </Nav.Link>
           </Nav.Item>
 
           <Nav.Item>
-            <Nav.Link as={Link} to='/wiki' active={location.pathname === '/wiki'} onClick={() => updateExpanded(false)}>
+            <Nav.Link as={Link} to='/wiki' active={location.pathname === "/wiki"} onClick={() => changeView(false)}>
               Wiki
             </Nav.Link>
           </Nav.Item>
 
           <Nav.Item>
-            <Nav.Link as={Link} to='/contacts' active={location.pathname === '/contacts'} onClick={() => updateExpanded(false)}>
+            <Nav.Link as={Link} to='/contacts' active={location.pathname === "/contacts"} onClick={() => changeView(false)}>
               Contacts
             </Nav.Link>
           </Nav.Item>
 
           <Nav.Item>
-            <Nav.Link as={Link} to='/library' active={location.pathname === '/library'} onClick={() => updateExpanded(false)}>
+            <Nav.Link as={Link} to='/library' active={location.pathname === "/library"} onClick={() => changeView(false)}>
               Library
             </Nav.Link>
           </Nav.Item>
