5fce4b7e869cf5f913edcd3a382f8e120a6a2241
Kya-Edmeade <119429408+Kya-Edmeade@users.noreply.github.com>
2023-02-10 11:37:41 +0000

    Contacts page useless code - removed

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ContactHeader from '../components/carousels/ContactHeader';
 import ContactsMainCarousel from '../components/carousels/contacts-team-carousel/ContactsMainCarousel';
 import ContactComponent from '../components/contact-component/ContactComponent';
-import { get } from '../service/MainCarouselService';
-import { useEffect, useState } from 'react';
 
 const Contacts = () => {
-  const [slideData, setSlideData] = useState([]);
-
-  useEffect(() => {
-    get('contact-header', setSlideData);
-  }, []);
   return (
     <div>
       <ContactHeader />
