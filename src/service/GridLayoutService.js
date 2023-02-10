33f71685406b72657443f9579997edb5b4d4910f
Kya-Edmeade <119429408+Kya-Edmeade@users.noreply.github.com>
2023-02-10 10:52:28 +0000

    baseURL - chnaged

--- src/service/GridLayoutService.js
+++ src/service/GridLayoutService.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const headers = {
   headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Credentials': 'true' },
 };
-// const baseURL = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/page-layout';
-const baseURL = 'http://localhost:3001/page-layout';
+const baseURL = 'http://ec-acad-elb-a07a79316f54cbbf.elb.eu-west-2.amazonaws.com:3001/page-layout';
+// const baseURL = 'http://localhost:3001/page-layout';
 
 const get = (setPage, setLayout, setIsLoading, location) => {
   axios.get(`${baseURL}?name=${location.loc}`, headers).then((res) => {
